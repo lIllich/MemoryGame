733941daa0ae780c727dd07327dac13d8c151ef6
luka <luka.illich2000@gmail.com>
2024-01-02 22:01:13 +0100

    commit

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from classes.ConfigManager import ConfigManager
 #!// bug - kada se close-a GameWindow u sred igre, javi exception
 #! bug - extended display, ako se window prikaze izvan dozovljenih granica (displaya) -> vrati ga
 
-#todo: dodati razine - easy, normal, hard, extra hard? (settings)
+#todo//: dodati razine - easy, normal, hard, extra hard? (settings)
 #todo: dodati varijablnog intervala za označavanje card (settings)
 #todo: dodati spremanje scora po razinama, u json se spremaju 10 najbojih po razini
 #todo: manager za dodavanje kategorija/kartica - text&text, text&slika, slika&slika
