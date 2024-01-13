8c811d824fd548116146c01701c2b40a5f05c85f
luka <luka.illich2000@gmail.com>
2024-01-13 15:36:35 +0100

    na sve windowe dodano ako su zbog extended dispaya ostali na nepostojecem drugom zaslonu, da se vrate na 10x10 poziciju

---
+++ classes/WindowManager.py
@@ -0,0 +1,24 @@
+
+def check_window_position(root):
+    # Update window and wait for all tasks to complete
+    root.update_idletasks()
+
+    # Get screen width and height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # Get window width, height, and position
+    window_width = root.winfo_width()
+    window_height = root.winfo_height()
+    window_x = root.winfo_x()
+    window_y = root.winfo_y()
+
+    # DEBUG
+    # print(screen_height, screen_width)
+    # print(window_x, window_y)
+    # print(window_height, window_width)
+
+    # Check if window is off screen
+    if window_x + window_width > screen_width or window_y + window_height > screen_height:
+        # Reset window position
+        root.geometry("+10+10")
